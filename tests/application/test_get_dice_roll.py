84bab6bbead19f2d526de33f6d9b81ed5a882022
andreavaras_sergioperez <andreavaras_sergioperez@outlook.com>
2025-12-03 16:40:00 +0100

    new test

--- tests/application/test_get_dice_roll.py
+++ tests/application/test_get_dice_roll.py
@@ -5,4 +5,4 @@ def test_result_is_a_valid_dice_value() -> None:
     for _ in range(100):
         result = get_dice_roll()
         assert isinstance(result, int)
-        assert result in {1, 2, 3, 4, 5, 6}
+        assert result in {7, 2, 3, 4, 5, 6}
